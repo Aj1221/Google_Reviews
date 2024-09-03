f0264ba15e9358655710f580dc4289d179a0f858
Aj1221 <aj.dude@rediff.com>
2024-09-03 20:38:26 +0530

    extra code removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
 import GoogleReviews from './Component/GoogleReviews/GoogleReviews';
 
 function App() {
   return (
     <div className="App">
       <GoogleReviews
-      //  placeId=
-      // "ChIJcYRyZWHvDDkRpJFpBp6YJ_A" 
+      
       apiKey="AIzaSyAfITX2OoKJO2Ge7fZLt9H8qOXOmVAVVLE" />
     </div>
   );
